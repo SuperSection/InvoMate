c50cdb5862e4a820619c14ae70a08227a04ebb4c
Soumo Sarkar <soumosarkar297@gmail.com>
2025-06-11 07:33:37 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Include logo image in public directory
    - Add welcome message to `App.tsx`
    - Add Menubar component
    - Add global styles in index.css with Tailwind and Urbanist font
    - Set up main entry point (main.tsx) for React application
    - Create Dashboard, LandingPage, and PreviewPage
    - Add TypeScript configuration files for app and node
    - Configure Vite with React and Tailwind CSS plugins

---
+++ invomate-client/src/App.tsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="font-urbanist text-3xl">
+        Welcome to InvoMate application
+      </h1>
+    </div>
+  )
+}
+
+export default App;
